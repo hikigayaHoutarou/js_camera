cb01b3c3cc8112bd78386526bbb36002b177c36b
hikigayaHoutarou <saiful@cayto.jp>
2023-01-14 00:43:30 +0600

    update 6

--- script.js
+++ script.js
@@ -8,24 +8,10 @@ const screenshotImage = document.querySelector('img');
 const buttons = [...controls.querySelectorAll('button')];
 
 let streamStarted = false;
-let facingMode = 'environment';
 
 const [play, pause, screenshot] = buttons;
 
 const constraints = {
-  video: {
-    width: {
-      min: 1280,
-      ideal: 1920,
-      max: 2560,
-    },
-    height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-    facingMode: facingMode
-  },
   audio: true
 };
 
@@ -38,7 +24,10 @@ play.onclick = () => {
   }
   if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
     const updatedConstraints = {
-      ...constraints
+      ...constraints,
+      video: {
+        facingMode: 'environment'
+      }
     };
     startStream(updatedConstraints);
   }
@@ -63,10 +52,14 @@ const handleStream = (stream) => {
 
 
 cameraOptions.onchange = (v) => {
-  facingMode = v;
   const updatedConstraints = {
-    ...constraints
+    ...constraints,
+    video: {
+      facingMode: cameraOptions.value
+    }
   };
+
+  console.log(updatedConstraints);
   startStream(updatedConstraints);
 };
 
